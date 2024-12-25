c98d404f2d368d1f97ccdbecc24924d3d8c13aa6
johnpapwinter <johnpapahimonas@yahoo.com>
2024-12-25 23:07:53 +0200

    rest logging

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+from src.articles.api import logging_middleware
 from src.articles.api.router import api_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.middleware("http")(logging_middleware)
+
 app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
---
+++ src/articles/api/logging_middleware.py
@@ -0,0 +1,20 @@
+from typing import Callable
+from fastapi import Request
+
+from src.articles.utils.logging import setup_logging
+
+logger = setup_logging(__name__)
+
+
+async def logger_middleware(request: Request, call_next: Callable) -> None:
+    logger.info(f"Request started: {request.method} {request.url}")
+
+    response = await call_next(request)
+
+    logger.info(
+        f"Request processed: {request.method} {request.url} "
+        f"- Status code: {response.status_code} "
+    )
+
+    return response
+
