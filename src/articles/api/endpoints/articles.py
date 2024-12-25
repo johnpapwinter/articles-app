bb51ed3d4d5cfa0b853e36e44b698694d0feec17
johnpapwinter <johnpapahimonas@yahoo.com>
2024-12-25 10:21:05 +0200

    cleaned code

--- src/articles/api/endpoints/articles.py
+++ src/articles/api/endpoints/articles.py
@@ -2,9 +2,7 @@ from typing import Any
 
 from fastapi import APIRouter, Depends
 
-from src.articles.api.deps import DbSession
-from src.articles.auth.deps import get_current_user
-from src.articles.models import User
+from src.articles.api.deps import DbSession, CurrentUser
 from src.articles.schemas.article import Article, ArticleCreate, ArticleUpdate
 from src.articles.services.article import ArticleService
 
@@ -12,12 +10,7 @@ article_router = APIRouter()
 
 
 @article_router.post("", response_model=Article)
-async def create_article(
-        *,
-        db: DbSession,
-        article_in:
-        ArticleCreate,
-        current_user: User = Depends(get_current_user)) -> Any:
+async def create_article(*, db: DbSession, article_in: ArticleCreate, current_user: CurrentUser) -> Any:
     article_service = ArticleService(db)
     article = await article_service.create(obj=article_in)
     return article
@@ -30,22 +23,13 @@ async def get_article(*, db: DbSession, article_id: int) -> Any:
 
 
 @article_router.put("/{article_id}", response_model=Article)
-async def update_article(
-        *,
-        db: DbSession,
-        article_id: int,
-        article: ArticleUpdate,
-        current_user: User = Depends(get_current_user)) -> Any:
+async def update_article(*, db: DbSession, article_id: int, article: ArticleUpdate, current_user: CurrentUser) -> Any:
     article_service = ArticleService(db)
     return await article_service.update(obj_id=article_id, obj=article)
 
 
 @article_router.delete("/{article_id}", response_model=Article)
-async def delete_article(
-        *,
-        db: DbSession,
-        article_id: int,
-        current_user: User = Depends(get_current_user)) -> Any:
+async def delete_article(*, db: DbSession, article_id: int, current_user: CurrentUser) -> Any:
     article_service = ArticleService(db)
     return await article_service.delete(obj_id=article_id)
 
--- src/articles/api/endpoints/comments.py
+++ src/articles/api/endpoints/comments.py
@@ -2,9 +2,7 @@ from typing import Any
 
 from fastapi import APIRouter, Depends
 
-from src.articles.api.deps import DbSession
-from src.articles.auth.deps import get_current_user
-from src.articles.models import User
+from src.articles.api.deps import DbSession, CurrentUser
 from src.articles.schemas.comment import Comment, CommentCreate, CommentUpdate
 from src.articles.services.comment import CommentService
 
@@ -12,11 +10,7 @@ comments_router = APIRouter()
 
 
 @comments_router.post("", response_model=Comment)
-async def create_comment(
-        *,
-        db: DbSession,
-        comment_in: CommentCreate,
-        current_user: User = Depends(get_current_user)) -> Any:
+async def create_comment(*, db: DbSession, comment_in: CommentCreate, current_user: CurrentUser) -> Any:
     comment_service = CommentService(db)
     comment = await comment_service.create(obj=comment_in)
     return comment
@@ -29,22 +23,13 @@ async def get_comment(*, db: DbSession, comment_id: int) -> Any:
 
 
 @comments_router.put("/{comment_id}", response_model=Comment)
-async def update_comment(
-        *,
-        db: DbSession,
-        comment_id: int,
-        comment_in: CommentUpdate,
-        current_user: User = Depends(get_current_user)) -> Any:
+async def update_comment(*, db: DbSession, comment_id: int, comment_in: CommentUpdate, current_user: CurrentUser) -> Any:
     comment_service = CommentService(db)
     return await comment_service.update(obj_id=comment_id, obj=comment_in)
 
 
 @comments_router.delete("/{comment_id}", response_model=Comment)
-async def delete_comment(
-        *,
-        db: DbSession,
-        comment_id: int,
-        current_user: User = Depends(get_current_user)) -> Any:
+async def delete_comment(*, db: DbSession, comment_id: int, current_user: CurrentUser) -> Any:
     comment_service = CommentService(db)
     return await comment_service.delete(obj_id=comment_id)
 
